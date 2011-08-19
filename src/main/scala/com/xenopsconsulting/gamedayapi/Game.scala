ff059a291bcd3fed13cb4654a6103fecd1813f8b
Ed Copony <ecopony@yahoo.com>
2011-08-18 21:43:58 -0700

    Finishing accessors for the Game API.

--- src/main/scala/com/xenopsconsulting/gamedayapi/Game.scala
+++ src/main/scala/com/xenopsconsulting/gamedayapi/Game.scala
@@ -19,13 +19,38 @@ class Game(date: Date, team: String) {
   def gameType():String = (gameNode \ "@type").text
   def localGameTime():String = (gameNode \ "@local_game_time").text
   def gamedaySw():String = (gameNode \ "@gameday_sw").text
-  def homeTeamCode():String = (teamNode("home") \ "@code").text
-  def awayTeamCode():String = (teamNode("away") \ "@code").text
-  
+  def homeTeamCode():String = (homeTeamNode \ "@code").text
+  def awayTeamCode():String = (awayTeamNode \ "@code").text
+  def homeTeamAbbrev():String = (homeTeamNode \ "@abbrev").text
+  def awayTeamAbbrev():String = (awayTeamNode \ "@abbrev").text
+  def homeTeamId():String = (homeTeamNode \ "@id").text
+  def awayTeamId():String = (awayTeamNode \ "@id").text
+  def homeTeamName():String = (homeTeamNode \ "@name").text
+  def awayTeamName():String = (awayTeamNode \ "@name").text
+  def homeTeamNameFull():String = (homeTeamNode \ "@name_full").text
+  def awayTeamNameFull():String = (awayTeamNode \ "@name_full").text
+  def homeTeamNameBrief():String = (homeTeamNode \ "@name_brief").text
+  def awayTeamNameBrief():String = (awayTeamNode \ "@name_brief").text
+  def homeTeamWins():String = (homeTeamNode \ "@w").text
+  def awayTeamWins():String = (awayTeamNode \ "@w").text
+  def homeTeamLosses():String = (homeTeamNode \ "@l").text
+  def awayTeamLosses():String = (awayTeamNode \ "@l").text
+  def homeTeamDivisionId():String = (homeTeamNode \ "@division_id").text
+  def awayTeamDivisionId():String = (awayTeamNode \ "@division_id").text
+  def homeTeamLeagueId():String = (homeTeamNode \ "@league_id").text
+  def awayTeamLeagueId():String = (awayTeamNode \ "@league_id").text
+  def homeTeamLeague():String = (homeTeamNode \ "@league").text
+  def awayTeamLeague():String = (awayTeamNode \ "@league").text
+  def stadiumId():String = (stadiumNode \ "@id").text
+  def stadiumName():String = (stadiumNode \ "@name").text
+  def location():String = (stadiumNode \ "@location").text
+
   private def fetch() = {
     _xml = fetch_strategy.fetch_game(date, team)
   }
 
   private def gameNode():NodeSeq = (xml \\ "game")
-  private def teamNode(name: String) = ((gameNode \ "team" ) find { _.attribute("type").get.text == name }).get
+  private def homeTeamNode = ((gameNode \ "team") find { _.attribute("type").get.text == "home" }).get
+  private def awayTeamNode = ((gameNode \ "team") find { _.attribute("type").get.text == "away" }).get
+  private def stadiumNode = (gameNode \ "stadium")
 }
--- src/test/scala/com/xenopsconsulting/gamedayapi/GameTest.scala
+++ src/test/scala/com/xenopsconsulting/gamedayapi/GameTest.scala
@@ -4,7 +4,6 @@ import org.scalatest.junit.AssertionsForJUnit
 import org.junit.Test
 import org.junit.Assert._
 import org.junit.Before
-import xml.{XML, Elem}
 import java.text.SimpleDateFormat
 import java.util.Date
 
@@ -12,7 +11,7 @@ class GameTest extends AssertionsForJUnit {
   var game: Game = _
   var date: Date = _
   var team: String = _
-  
+
   @Before def initialize {
     date = new SimpleDateFormat("yyy-MM-dd").parse("2011-08-13")
     team = "sfn"
@@ -28,7 +27,7 @@ class GameTest extends AssertionsForJUnit {
   @Test def testGameTime {
     assertEquals("10:10 PM", game.gameTimeEt)
   }
-  
+
   @Test def testGameType {
     assertEquals("R", game.gameType)
   }
@@ -49,18 +48,96 @@ class GameTest extends AssertionsForJUnit {
     assertEquals("bos", game.awayTeamCode)
   }
 
-    /*
-    TODO:
-      - Team bits
-      - Stadium bits
+  @Test def testHomeTeamAbbrev {
+    assertEquals("SEA", game.homeTeamAbbrev)
+  }
 
-      <game type="R" local_game_time="19:10" game_pk="288682" game_time_et="10:10 PM" gameday_sw="P">
-        <team type="home" code="sea" file_code="sea" abbrev="SEA" id="136" name="Seattle" name_full="Seattle Mariners" name_brief="Mariners" w="51" l="67" division_id="200" league_id="103" league="AL"/>
-        <team type="away" code="bos" file_code="bos" abbrev="BOS" id="111" name="Boston" name_full="Boston Red Sox" name_brief="Red Sox" w="73" l="45" division_id="201" league_id="103" league="AL"/>
-        <stadium id="680" name="Safeco Field" venue_w_chan_loc="USWA0395" location="Seattle, WA"/>
-      </game>
-   */
+  @Test def testAwayTeamAbbrev {
+    assertEquals("BOS", game.awayTeamAbbrev)
+  }
+
+  @Test def testHomeTeamId {
+    assertEquals("136", game.homeTeamId)
+  }
+
+  @Test def testAwayTeamId {
+    assertEquals("111", game.awayTeamId)
+  }
+
+  @Test def testHomeTeamName {
+    assertEquals("Seattle", game.homeTeamName)
+  }
+
+  @Test def testAwayTeamName {
+    assertEquals("Boston", game.awayTeamName)
+  }
+  
+  @Test def testHomeTeamNameFull {
+    assertEquals("Seattle Mariners", game.homeTeamNameFull)
+  }
+
+  @Test def testAwayTeamNameFull {
+    assertEquals("Boston Red Sox", game.awayTeamNameFull)
+  }
+
+  @Test def testHomeTeamNameBrief {
+    assertEquals("Mariners", game.homeTeamNameBrief)
+  }
+
+  @Test def testAwayTeamNameBrief {
+    assertEquals("Red Sox", game.awayTeamNameBrief)
+  }
+
+  @Test def testHomeTeamWins {
+    assertEquals("51", game.homeTeamWins)
+  }
+
+  @Test def testAwayTeamWins {
+    assertEquals("73", game.awayTeamWins)
+  }
+
+  @Test def testHomeTeamLosses {
+    assertEquals("67", game.homeTeamLosses)
+  }
+
+  @Test def testAwayTeamLosses {
+    assertEquals("45", game.awayTeamLosses)
+  }
+
+  @Test def testHomeTeamDivisionId {
+    assertEquals("200", game.homeTeamDivisionId)
+  }
+
+  @Test def testAwayTeamDivisionId {
+    assertEquals("201", game.awayTeamDivisionId)
+  }
+
+  @Test def testHomeTeamLeagueId {
+    assertEquals("103", game.homeTeamLeagueId)
+  }
+
+  @Test def testAwayTeamLeagueId {
+    assertEquals("103", game.awayTeamLeagueId)
+  }
+
+  @Test def testHomeTeamLeague {
+    assertEquals("AL", game.homeTeamLeague)
+  }
+
+  @Test def testAwayTeamLeague {
+    assertEquals("AL", game.awayTeamLeague)
+  }
+
+  @Test def testStadiumId {
+    assertEquals("680", game.stadiumId)
+  }
+
+  @Test def testStadiumName {
+    assertEquals("Safeco Field", game.stadiumName)
+  }
+
+  @Test def testLocation {
+    assertEquals("Seattle, WA", game.location)
+  }
+  
 }
-
-
-
