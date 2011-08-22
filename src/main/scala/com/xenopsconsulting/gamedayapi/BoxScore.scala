63218feead9a832ba21c5b97b3aff16f2ddabd8f
Ed Copony <ecopony@yahoo.com>
2011-08-21 21:56:40 -0700

    Adding the boxscore element attributes.

--- src/main/scala/com/xenopsconsulting/gamedayapi/BoxScore.scala
+++ src/main/scala/com/xenopsconsulting/gamedayapi/BoxScore.scala
@@ -12,6 +12,22 @@ class BoxScore(date: Date, team: String) extends XmlRepresentation {
   }
 
   def gameId():String = (boxScoreNode \ "@game_id").text
+  def gamePk():String = (boxScoreNode \ "@game_pk").text
+  def venueId():String = (boxScoreNode \ "@venue_id").text
+  def venueName():String = (boxScoreNode \ "@venue_name").text
+  def homeSportCode():String = (boxScoreNode \ "@home_sport_code").text
+  def homeTeamCode():String = (boxScoreNode \ "@home_team_code").text
+  def awayTeamCode():String = (boxScoreNode \ "@away_team_code").text
+  def homeId():String = (boxScoreNode \ "@home_id").text
+  def awayId():String = (boxScoreNode \ "@away_id").text
+  def homeFname():String = (boxScoreNode \ "@home_fname").text
+  def awayFname():String = (boxScoreNode \ "@away_fname").text
+  def homeSname():String = (boxScoreNode \ "@home_sname").text
+  def homeWins():String = (boxScoreNode \ "@home_wins").text
+  def homeLoss():String = (boxScoreNode \ "@home_loss").text
+  def awayWins():String = (boxScoreNode \ "@away_wins").text
+  def awayLoss():String = (boxScoreNode \ "@away_loss").text
+  def statusInd():String = (boxScoreNode \ "@status_ind").text
 
   private def boxScoreNode():NodeSeq = (xml \\ "boxscore")
 }
--- src/test/scala/com/xenopsconsulting/gamedayapi/BoxScoreTest.scala
+++ src/test/scala/com/xenopsconsulting/gamedayapi/BoxScoreTest.scala
@@ -20,7 +20,76 @@ class BoxScoreTest extends AssertionsForJUnit {
   }
 
   @Test def testInitialization {
+    assertEquals(team, boxScore.tm)
+    assertEquals(date, boxScore.dt)
+  }
+
+  @Test def testGameId {
     assertEquals("2011/08/13/bosmlb-seamlb-1", boxScore.gameId)
+  }
+
+  @Test def testGamePk {
+    assertEquals("288682", boxScore.gamePk)
+  }
+
+  @Test def testVenueId {
+    assertEquals("680", boxScore.venueId)
+  }
+
+  @Test def testVenueName {
+    assertEquals("Safeco Field", boxScore.venueName)
+  }
+
+  @Test def testHomeSportCode {
+    assertEquals("mlb", boxScore.homeSportCode)
+  }
+
+  @Test def testAwayTeamCode {
+    assertEquals("bos", boxScore.awayTeamCode)
+  }
+
+  @Test def testHomeTeamCode {
+    assertEquals("sea", boxScore.homeTeamCode)
+  }
+
+  @Test def testAwayId {
+    assertEquals("111", boxScore.awayId)
+  }
+
+  @Test def testHomeId {
+    assertEquals("136", boxScore.homeId)
+  }
+
+  @Test def testAwayFname {
+    assertEquals("Boston Red Sox", boxScore.awayFname)
+  }
+
+  @Test def testHomeFname {
+    assertEquals("Seattle Mariners", boxScore.homeFname)
+  }
+
+  @Test def testHomeSname {
+    assertEquals("Seattle", boxScore.homeSname)
+  }
+
+  @Test def testAwayWins {
+    assertEquals("73", boxScore.awayWins)
+  }
+
+  @Test def testAwayLoss {
+    assertEquals("45", boxScore.awayLoss)
+  }
+
+  @Test def testHomeWins {
+    assertEquals("51", boxScore.homeWins)
+  }
+
+  @Test def testHomeLoss {
+    assertEquals("67", boxScore.homeLoss)
+  }
+
+  @Test def testStatusInd {
+    assertEquals("F", boxScore.statusInd)
   }
 
 }
