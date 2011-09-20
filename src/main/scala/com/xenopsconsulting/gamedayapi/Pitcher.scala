25de4dc98f67c39cbec74f18d063faa51dbf7064
Ed Copony <ecopony@yahoo.com>
2011-09-19 19:21:02 -0700

    Filling in the pitcher attributes.

--- src/main/scala/com/xenopsconsulting/gamedayapi/Pitcher.scala
+++ src/main/scala/com/xenopsconsulting/gamedayapi/Pitcher.scala
@@ -5,5 +5,26 @@ import xml.Node
 class Pitcher(pitcherNode: Node) {
   def pn = pitcherNode
 
-  def name():String = (pitcherNode \ "@name").text
+  def id():String = pna("@id")
+  def name():String = pna("@name")
+  def nameDisplayFirstLast():String = pna("@name_display_first_last")
+  def pos():String = pna("@pos")
+  def out():String = pna("@out")
+  def bf():String = pna("@bf")
+  def er():String = pna("@er")
+  def r():String = pna("@r")
+  def h():String = pna("@h")
+  def so():String = pna("@so")
+  def hr():String = pna("@hr")
+  def bb():String = pna("@bb")
+  def w():String = pna("@w")
+  def l():String = pna("@l")
+  def sv():String = pna("@sv")
+  def bs():String = pna("@bs")
+  def hld():String = pna("@hld")
+  def era():String = pna("@era")
+  def note():String = pna("@note")
+
+  private def pna(attribute: String):String = (pitcherNode \ attribute).text
+
 }
--- src/test/scala/com/xenopsconsulting/gamedayapi/PitcherTest.scala
+++ src/test/scala/com/xenopsconsulting/gamedayapi/PitcherTest.scala
@@ -12,8 +12,80 @@ class PitcherTest extends AssertionsForJUnit {
     pitcher = new Pitcher(<pitcher id="277417" name="Beckett" name_display_first_last="Josh Beckett" pos="P" out="15" bf="23" er="5" r="5" h="8" so="6" hr="2" bb="1" w="9" l="5" sv="0" bs="0" hld="0" era="2.40" note="(L, 9-5)"/>)
   }
 
+  @Test def testId {
+    assertEquals("277417", pitcher.id)
+  }
+
   @Test def testName {
     assertEquals("Beckett", pitcher.name)
+  }
+
+  @Test def testNameDisplayFirstLast {
+    assertEquals("Josh Beckett", pitcher.nameDisplayFirstLast)
+  }
+
+  @Test def testPos {
+    assertEquals("P", pitcher.pos)
+  }
+
+  @Test def testOut {
+    assertEquals("15", pitcher.out)
+  }
+
+  @Test def testBf {
+    assertEquals("23", pitcher.bf)
+  }
+
+  @Test def testEr {
+    assertEquals("5", pitcher.er)
+  }
+
+  @Test def testR {
+    assertEquals("5", pitcher.r)
+  }
+
+  @Test def testH {
+    assertEquals("8", pitcher.h)
+  }
+
+  @Test def testSo {
+    assertEquals("6", pitcher.so)
+  }
+
+  @Test def testHr {
+    assertEquals("2", pitcher.hr)
+  }
+
+  @Test def testBb {
+    assertEquals("1", pitcher.bb)
+  }
+
+  @Test def testW {
+    assertEquals("9", pitcher.w)
+  }
+
+  @Test def testL {
+    assertEquals("5", pitcher.l)
+  }
+
+  @Test def testSv {
+    assertEquals("0", pitcher.sv)
+  }
+
+  @Test def testBs {
+    assertEquals("0", pitcher.bs)
+  }
+
+  @Test def testHld {
+    assertEquals("0", pitcher.hld)
+  }
+
+  @Test def testEra {
+    assertEquals("2.40", pitcher.era)
+  }
+
+  @Test def testNote {
+    assertEquals("(L, 9-5)", pitcher.note)
   }
 
 }
